4cd0542ed2f1b0f3d18ac266ac20d0f76d8609c1
Marques Scripps <hello@mscripps.com>
2026-01-10 21:28:17 +0000

    chore: Added support for renovate

--- commitlint.config.js
+++ commitlint.config.js
@@ -25,34 +25,5 @@ module.exports = {
     "type-empty": [2, "never"],
     // Disable subject-case to allow Linear ticket IDs (STE-XXX)
     "subject-case": [0],
-    // Subject must start with Linear ticket ID
-    "subject-linear-ticket": [2, "always"],
   },
-  plugins: [
-    {
-      rules: {
-        "subject-linear-ticket": ({ subject }) => {
-          if (!subject) {
-            return [false, "Subject cannot be empty"];
-          }
-
-          // Check if subject starts with STE-XXX format
-          const linearTicketRegex = /^STE-\d+\s+.+/;
-          const isValid = linearTicketRegex.test(subject);
-
-          if (!isValid) {
-            return [
-              false,
-              'Subject must start with Linear ticket ID in format: "STE-XXX description"\n' +
-                "Example: feat: STE-123 add user authentication\n" +
-                "\n" +
-                "Get your ticket ID from: https://linear.app/stellarstack/issue/",
-            ];
-          }
-
-          return [true, ""];
-        },
-      },
-    },
-  ],
 };
